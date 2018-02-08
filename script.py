593a64b8f7d70f17a1b99421f1dccfbd8072e0a0
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-02-09 05:06:00 +0530

    Made functions for scraping the user's starred repositories

--- script.py
+++ script.py
@@ -7,13 +7,31 @@ client = requests.get(url)
 page_soup = soup(client.content, 'html.parser')
 detailed_repos = page_soup.findAll('div', {'id':'user-repositories-list'})
 
+# functions for scraping my starred repos
+
+def detailed_starred_repos(username):
+    client = requests.get("http://www.github.com/" + username + "?tab=stars")
+    page_soup = soup(client.content, 'html.parser')
+    starred_repos = page_soup.findAll('div', {'class': 'position-relative'}).findAll('div')
+    return starred_repos[1:]
+
+def starred_repos_titles(username):
+    repos = detailed_starred_repos(username)
+    titles = [repo.find('div').find('h3').find('a').span.text + repo.find('div').find('h3').find('a').text for repo in repos]
+    return titles
+
+def starred_repos_descriptions(username):
+    repos = detailed_starred_repos(username)
+    descriptions = [repo.findAll('div')[1].find('p').text for repo in repos]
+    return descriptions
+
 # functions for scraping my repos
 
 def detailed_repos(username):
     client = requests.get("http://www.github.com/" + username + "?tab=repositories")
     page_soup = soup(client.content, 'html.parser')
-    detailed_repos = page_soup.findAll('div', {'id': 'user-repositories-list'})[0].find('ul').findAll('li')
-    return detailed_repos
+    repos = page_soup.findAll('div', {'id': 'user-repositories-list'})[0].find('ul').findAll('li')
+    return repos
 
 def repos_titles(username):
     repos_detailed = detailed_repos(username)
